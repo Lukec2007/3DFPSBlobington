92761190e19a961b0b3f18570c7274348fc6fdfe
Lukec2007 <146829004+Lukec2007@users.noreply.github.com>
2023-12-07 16:24:59 +0000

    vIFORGOT

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,41 +6,43 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     public NavMeshAgent agent;
-
     public Transform player;
-
     public LayerMask whatIsGround, whatIsPlayer;
 
     public float health;
-
-    public Vector3 walkPoint;
-    bool walkPointSet;
+    public float sightRange, attackRange;
     public float walkPointRange;
-
     public float timeBetweenAttacks;
-    bool alreadyAttacked;
+
+    // Patroling
+    private Vector3 walkPoint;
+    private bool walkPointSet;
+
+    // Attacking
+    private bool alreadyAttacked;
     public GameObject projectile;
 
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
+    private bool playerInSightRange;
+    private bool playerInAttackRange;
 
     private void Awake()
     {
-        player = GameObject.Find("PlayerObj").transform;
+        player = GameObject.Find("FirstPersonPlayer").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
+        // Check for sight and attack range
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
         if (playerInSightRange && !playerInAttackRange) ChasePlayer();
         if (playerInAttackRange && playerInSightRange) AttackPlayer();
     }
-  
-    private void Patrolling()
+
+    private void Patroling()
     {
         if (!walkPointSet) SearchWalkPoint();
 
@@ -49,12 +51,14 @@ public class EnemyAI : MonoBehaviour
 
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
-        if (distanceToWalkPoint.magnitude < 1f)
+        // Walkpoint reached
+        if (distanceToWalkPoint.sqrMagnitude < 1f)
             walkPointSet = false;
     }
 
     private void SearchWalkPoint()
     {
+        // Calculate random point in range
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
@@ -71,21 +75,24 @@ public class EnemyAI : MonoBehaviour
 
     private void AttackPlayer()
     {
+        // Make sure enemy doesn't move
         agent.SetDestination(transform.position);
 
         transform.LookAt(player);
 
         if (!alreadyAttacked)
         {
+            // Attack code here
             Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-
             rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
             rb.AddForce(transform.up * 8f, ForceMode.Impulse);
 
+            // End of attack code
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
     }
+
     private void ResetAttack()
     {
         alreadyAttacked = false;
@@ -95,11 +102,19 @@ public class EnemyAI : MonoBehaviour
     {
         health -= damage;
 
-        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+        if (health <= 0) Invoke("DestroyEnemy", 0.5f);
     }
 
     private void DestroyEnemy()
     {
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
